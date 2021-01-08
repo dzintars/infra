8bed4088f767504b6343406fd0f1b7b0c2b4c781
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-01-08 19:36:49 +0200

    fix(terraform): add vault secrets

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -4,11 +4,11 @@ terraform {
     bucket                      = "terraform"
     key                         = "dev/terraform.tfstate"
     region                      = "eu-north-1"
-    /* skip_requesting_account_id  = true */
-    /* skip_credentials_validation = true */
-    /* skip_get_ec2_platforms      = true */
-    /* skip_metadata_api_check     = true */
-    /* skip_region_validation      = true */
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
     force_path_style            = true
   }
 }
