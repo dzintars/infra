87d68f56e1f73d3eb04ff1a89ed8bfb95bf6ba27
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-01-11 23:48:20 +0200

    fix(tf:vault): update policies

--- terraform/modules/vault/policies.tf
+++ terraform/modules/vault/policies.tf
@@ -1,15 +1,15 @@
 resource "vault_policy" "admin_policy" {
   name   = "admins"
-  policy = "${data.local_file.admin_policy.content}"
+  policy = data.local_file.admin_policy.content
 }
 
 resource "vault_policy" "developer_policy" {
   name   = "developers"
-  policy = "${data.local_file.developer_policy.content}"
+  policy = data.local_file.developer_policy.content
 }
 
 resource "vault_policy" "operations_policy" {
   name   = "operations"
-  policy = "${data.local_file.operations_policy.content}"
+  policy = data.local_file.operations_policy.content
 }
 
