2dc6236106eb62e5bbfc858f6b8a6a0d6d85a484
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-01-15 18:46:28 +0200

    fix(vault): add terraform role

--- terraform/modules/vault/roles.tf
+++ terraform/modules/vault/roles.tf
@@ -1,1 +1,11 @@
+resource "vault_approle_auth_backend_role" "terraform" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "terraform"
+  token_policies = [vault_policy.terraform.name]
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "terraform" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.terraform.role_name
+}
 
