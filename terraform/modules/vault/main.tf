1bba07b71094f558043f5d5b1289def889ba593e
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-01-21 23:08:59 +0200

    feat(vault): remove duplicated approle resource

--- terraform/modules/vault/main.tf
+++ terraform/modules/vault/main.tf
@@ -2,10 +2,10 @@ resource "vault_auth_backend" "userpass" {
   type = "userpass"
 }
 
-resource "vault_auth_backend" "approle" {
-  type = "approle"
-  path = "approle"
-}
+# resource "vault_auth_backend" "approle" {
+#   type = "approle"
+#   path = "approle"
+# }
 
 resource "vault_mount" "developers" {
   path        = "developers"
--- terraform/modules/vault/roles.tf
+++ terraform/modules/vault/roles.tf
@@ -4,8 +4,12 @@ resource "vault_approle_auth_backend_role" "terraform" {
   token_policies = [vault_policy.terraform.name]
 }
 
+data "vault_auth_backend" "approle" {
+  path = "approle"
+}
+
 resource "vault_approle_auth_backend_role_secret_id" "terraform" {
-  backend   = vault_auth_backend.approle.path
+  backend   = data.vault_auth_backend.approle.path
   role_name = vault_approle_auth_backend_role.terraform.role_name
 }
 
