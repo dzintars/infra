d618d68ee3d194d9858956cff779463d872b8f57
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-01-21 23:11:32 +0200

    feat(vault): fix backend path

--- terraform/modules/vault/roles.tf
+++ terraform/modules/vault/roles.tf
@@ -1,11 +1,11 @@
-resource "vault_approle_auth_backend_role" "terraform" {
-  backend        = vault_auth_backend.approle.path
-  role_name      = "terraform"
-  token_policies = [vault_policy.terraform.name]
-}
-
 data "vault_auth_backend" "approle" {
   path = "approle"
+}
+
+resource "vault_approle_auth_backend_role" "terraform" {
+  backend        = data.vault_auth_backend.approle.path
+  role_name      = "terraform"
+  token_policies = [vault_policy.terraform.name]
 }
 
 resource "vault_approle_auth_backend_role_secret_id" "terraform" {
