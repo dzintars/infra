84c2aafad299f1074cb5231c7e97f0a7f25b7e63
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-01-22 20:52:28 +0200

    feat(domain): inject variables

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -15,11 +15,14 @@ module "libvirt_network" {
 module "bastion" {
   source = "../../modules/libvirt-domain"
 
-  pool_name      = module.libvirt_pool.name
-  network_id     = module.libvirt_network.id
-  network_name   = module.libvirt_network.name
-  img_url        = "https://mirror.netsite.dk/fedora/linux/releases/33/Cloud/x86_64/images/Fedora-Cloud-Base-33-1.2.x86_64.qcow2"
-  instance_count = 1
+  pool_name       = module.libvirt_pool.name
+  network_id      = module.libvirt_network.id
+  network_name    = module.libvirt_network.name
+  img_url         = "https://mirror.netsite.dk/fedora/linux/releases/33/Cloud/x86_64/images/Fedora-Cloud-Base-33-1.2.x86_64.qcow2"
+  instance_count  = 1
+  vault_addr      = "https://vault.oswee.com"
+  vault_role_id   = module.vault-ssh.approle.role
+  vault_secret_id = module.vault-ssh.approle.secret
 }
 
 module "vault" {
