c958bdc991d8d5dcaddfe91de5a40ee3a0a60ef6
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-02-02 20:23:38 +0200

    feat(vault): add jenkins approle

--- terraform/modules/vault-ssh/approles.tf
+++ terraform/modules/vault-ssh/approles.tf
@@ -16,10 +16,24 @@ data "vault_policy_document" "instance" {
   }
 }
 
+data "vault_policy_document" "jekins" {
+  rule {
+    path         = "ssh-client-signer/sign/clientrole"
+    capabilities = ["update"]
+    description  = "Allow Jenkins to sign its own ssh certs"
+  }
+}
+
 # Create the policy
 resource "vault_policy" "instance" {
   name   = "instance"
   policy = data.vault_policy_document.instance.hcl
+}
+
+# Create the Jenkins policy
+resource "vault_policy" "jenkins" {
+  name   = "jenkins"
+  policy = data.vault_policy_document.jenkins.hcl
 }
 
 # Create the role and assign the policy
@@ -29,9 +43,21 @@ resource "vault_approle_auth_backend_role" "instance" {
   token_policies = ["default", vault_policy.instance.name]
 }
 
+# Create the Jenkins role and assign the policy
+resource "vault_approle_auth_backend_role" "jenkins" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "jenkins"
+  token_policies = [vault_policy.jenkins.name]
+}
+
 # Create the secret which to inject into instance
 resource "vault_approle_auth_backend_role_secret_id" "instance" {
   backend = vault_auth_backend.approle.path
   role_name = vault_approle_auth_backend_role.instance.role_name
 }
 
+# Create the secret which to inject into Jenkins
+resource "vault_approle_auth_backend_role_secret_id" "jenkins" {
+  backend = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.jenkins.role_name
+}
